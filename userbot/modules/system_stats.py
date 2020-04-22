7de6c826f2b3ea1d5a16da9497c3c68c5122a98b
arifap195 <63351563+arifap195@users.noreply.github.com>
2020-04-22 13:58:34 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -122,8 +122,8 @@ async def pipcheck(pip):
 @register(outgoing=True, pattern=r"^\.(?:alive|on)\s?(.)?")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit    
-                     (" `•O N `\n"
+    await alive.edit(" `* On `\n
+
                          "================================ \n"
                      f"* `Telethon : v{version.__version__} `\n"
 
