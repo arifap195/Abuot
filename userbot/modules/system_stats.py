5c5de659d75d88cd80c8875190e5eabfbb12b1c2
arifap195 <63351563+arifap195@users.noreply.github.com>
2020-04-22 14:10:06 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -122,17 +122,17 @@ async def pipcheck(pip):
 @register(outgoing=True, pattern=r"^\.(?:alive|on)\s?(.)?")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit(" `* On `\n
-
-                         "================================ \n"
-                     f"* `Telethon : v{version.__version__} `\n"
-
-                     f"* `Python   : v{python_version()} `\n"
-	                 "================================ \n"
-                     f"* `Username : `{DEFAULTUSER} \n"
-		         "================================ \n"
-		     f"* `Project Abuot `\n"
-		         "================================ \n")
+    await alive.edit("`* On `\n
+                         "÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷ \n"
+                      f"* `Telethon : v{version.__version__} `\n"
+                      f"* `Python   : v{python_version()} `\n"
+                         "÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷ \n"
+                      f"* `Username : `{DEFAULTUSER} \n"
+	
+                         "÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷ \n"
+		f"* `Project Abuot `\n"
+                         "÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷ \n"
+		       
 	
 
 
